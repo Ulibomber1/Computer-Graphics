f6d3bedd4528c012279f5883069f061017cee356
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2024-02-01 00:36:26 -0800

    Initial Commit 2

---
+++ CSE4200_Lab1_(House)/CSE4200_Lab1_(House)/CSE4200_Lab1_(House).cpp
@@ -0,0 +1,14 @@
+﻿// CSE4200_Lab1_(House).cpp : Defines the entry point for the application.
+//
+
+#include "CSE4200_Lab1_(House).h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+
+
+	return 0;
+}
---
+++ CSE4200_Lab1_(House)/CSE4200_Lab1_(House)/CSE4200_Lab1_(House).h
@@ -0,0 +1,8 @@
+﻿// CSE4200_Lab1_(House).h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <glut.h>
+// TODO: Reference additional headers your program requires here.
