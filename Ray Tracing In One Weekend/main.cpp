71de77213ae97885efcaffde70c7a26cb51ce133
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2024-05-02 00:08:55 -0700

    6.2: Simplifying the Ray-Sphere Intersection Code

--- Ray Tracing In One Weekend/main.cpp
+++ Ray Tracing In One Weekend/main.cpp
@@ -9,18 +9,18 @@ double hit_sphere(const point3& center, double radius, const ray& r)
     vec3 oc = center - r.origin(); // the arbitrary point where the center is located
 
     // components of the quadratic formula
-    auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius * radius;
+    auto a = r.direction().length_squared();
+    auto h = dot(r.direction(), oc); // b = -2h, so -2 is gone
+    auto c = oc.length_squared() - radius * radius;
 
     // solve for the discriminant value
-    auto discriminant = b * b - 4 * a * c;
+    auto discriminant = h * h - a * c;
 
     if (discriminant < 0){
         return -1.0;
     }
     else {
-        return (-b - sqrt(discriminant)) / (2.0 * a);
+        return (h - sqrt(discriminant)) / a;
     }
 }
 
