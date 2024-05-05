f3103aabaef5156fed9c4c7b9ffc32db148f454d
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2024-05-04 22:03:51 -0700

    12.1: Camera Viewing Geometry

--- Ray Tracing In One Weekend/camera.h
+++ Ray Tracing In One Weekend/camera.h
@@ -13,6 +13,8 @@ public:
     int samples_per_pixel = 10;
     int max_depth = 10; // Max ray bounce depth
 
+    double vfov = 90; // vertical view angle
+
     void render(const hittable& world)
     {
         initialize();
@@ -53,7 +55,9 @@ private:
 
         // Determine viewport dimensions
         auto focal_length = 1.0;
-        auto viewport_height = 2.0;
+        auto theta = degrees_to_radians(vfov);
+        auto h = tan(theta / 2);
+        auto viewport_height = 2 * h * focal_length;
         auto viewport_width = viewport_height * (double(image_width) / image_height);
 
         // Calculate the vectors across the horizontal and down the vertical viewport edges.
