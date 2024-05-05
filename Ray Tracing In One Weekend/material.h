70fc8e5440006c77530d25ffd81a91b2259339cb
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2024-05-04 21:28:29 -0700

    11.4: Shlick Approximation

--- Ray Tracing In One Weekend/material.h
+++ Ray Tracing In One Weekend/material.h
@@ -74,7 +74,7 @@ public:
         bool cannot_refract = ri * sin_theta > 1.0;
         vec3 direction;
 
-        if (cannot_refract)
+        if (cannot_refract || reflectance(cos_theta, ri) > random_double())
             direction = reflect(unit_direction, rec.normal);
         else
             direction = refract(unit_direction, rec.normal, ri);
@@ -82,8 +82,17 @@ public:
         scattered = ray(rec.p, direction);
         return true;
     }
+
 private:
     double refraction_index; // Refractive index (RI) of mat in air or vacuum, or the ratio of two RI's (outer/inner)
+
+    static double reflectance(double cosine, double refraction_index)
+    {
+        // Use Shlick's approximation for reflectance.
+        auto r0 = (1 - refraction_index) / (1 + refraction_index);
+        r0 = r0 * r0;
+        return r0 + (1 - r0) * pow((1 - cosine), 5);
+    }
 };
 
 #endif
